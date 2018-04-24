5c203fb46c45d7da6bdbfba3171a0aa1c4c92cac
Matt Deuschle <mdeuschle@gmail.com>
2018-04-23 18:17:47 -0700

    data model

---
+++ Ofnote/Model/Note.swift
@@ -0,0 +1,18 @@
+//
+//  Note.swift
+//  Ofnote
+//
+//  Created by Matt Deuschle on 4/23/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import Foundation
+
+struct Note: Codable {
+    let title: String
+    let dateCreated = Date()
+
+    init?(title: String) {
+        self.title = title
+    }
+}
