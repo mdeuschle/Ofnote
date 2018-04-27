1609a00d1bcfead4b0de4f021ad0e937c55bd360
Matt Deuschle <mdeuschle@gmail.com>
2018-04-26 22:14:19 -0700

    Add note add view and delegation

---
+++ Ofnote/Controller/AddNoteVC.swift
@@ -0,0 +1,48 @@
+//
+//  AddNoteVC.swift
+//  Ofnote
+//
+//  Created by Matt Deuschle on 4/26/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+protocol AddNoteDelegate {
+    func add(note: Note)
+}
+
+import UIKit
+
+class AddNoteVC: UIViewController {
+
+    @IBOutlet weak var addNoteTextField: UITextField!
+    var delegate: AddNoteDelegate?
+
+    init(delegate: AddNoteDelegate) {
+        self.delegate = delegate
+        super.init(nibName: "AddNoteVC", bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addNoteTextField.delegate = self
+    }
+}
+
+extension AddNoteVC: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addNoteTextField.resignFirstResponder()
+        if let delegate = delegate,
+            let textFieldText = addNoteTextField.text,
+            let note = Note(title: textFieldText) {
+            delegate.add(note: note)
+            navigationController?.popViewController(animated: true)
+        }
+        return true
+    }
+}
+
--- Ofnote/Controller/RootVC.swift
+++ Ofnote/Controller/RootVC.swift
@@ -8,17 +8,53 @@
 
 import UIKit
 
-class RootVC: UIViewController {
+class RootVC: UIViewController, AddNoteDelegate {
+
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var addNoteToolBar: UIToolbar!
 
+    private var notes = [Note]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
     }
-    
-    @IBAction func addNoteTapped(_ sender: UIBarButtonItem) {
 
+    func add(note: Note) {
+        notes.append(note)
+        tableView.reloadData()
+    }
+
+    @IBAction func addNoteTapped(_ sender: UIBarButtonItem) {
+        let addNoteVC = AddNoteVC(delegate: self)
+        addNoteVC.delegate = self
+        navigationController?.pushViewController(addNoteVC, animated: true)
     }
 }
+
+extension RootVC: UITableViewDataSource {
+
+    //MARK: TableViewDataSource Methods
+
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "NoteCell")
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note.title
+        return cell
+    }
+
+}
+
+
+
+
+
 
