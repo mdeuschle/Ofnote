fc2e99ba988de4feedd13a29201c80740724e1a8
Matt Deuschle <mdeuschle@gmail.com>
2018-04-28 11:53:33 -0700

    filtered search

--- Ofnote/Controller/RootVC.swift
+++ Ofnote/Controller/RootVC.swift
@@ -13,8 +13,12 @@ class RootVC: UIViewController, AddNoteDelegate {
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var addNoteToolBar: UIToolbar!
 
+    // MARK: Private Properties
     private var notes = [Note]()
+    private var filteredNotes = [Note]()
+    private var isFiltering = false
 
+    // MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
@@ -28,6 +32,7 @@ class RootVC: UIViewController, AddNoteDelegate {
         tableView.reloadData()
     }
 
+    // MARK: Actions
     @IBAction func addNoteTapped(_ sender: UIBarButtonItem) {
         let addNoteVC = AddNoteVC(delegate: self)
         addNoteVC.delegate = self
@@ -37,30 +42,41 @@ class RootVC: UIViewController, AddNoteDelegate {
 
 extension RootVC: UITableViewDataSource {
 
-    //MARK: TableViewDataSource Methods
-
+    //MARK: TableViewDataSource
     public func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notes.count
+        return isFiltering ? filteredNotes.count : notes.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "NoteCell")
-        let note = notes[indexPath.row]
+        let note = isFiltering ? filteredNotes[indexPath.row] : notes[indexPath.row]
         cell.textLabel?.text = note.title
         return cell
     }
 }
 
-extension RootVC: UISearchControllerDelegate {
+extension RootVC: UISearchControllerDelegate, UISearchResultsUpdating {
 
     private func configureSearchBar() {
         let searchBarController = UISearchController(searchResultsController: nil)
         searchBarController.delegate = self
+        searchBarController.searchResultsUpdater = self
         navigationItem.searchController = searchBarController
+    }
+
+    func updateSearchResults(for searchController: UISearchController) {
+        if let text = searchController.searchBar.text, !text.isEmpty {
+            isFiltering = true
+            filteredNotes = notes.filter { $0.title.lowercased().contains(text.lowercased()) }
+        } else {
+            isFiltering = false
+            filteredNotes = notes
+        }
+        tableView.reloadData()
     }
 }
 
