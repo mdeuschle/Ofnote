551239daba89768c6213e00697f8aae0340ec435
Matt Deuschle <mdeuschle@gmail.com>
2018-04-29 15:17:49 -0700

    add date picker

--- Ofnote/Controller/AddNoteVC.swift
+++ Ofnote/Controller/AddNoteVC.swift
@@ -15,8 +15,10 @@ import UIKit
 class AddNoteVC: UIViewController {
 
     @IBOutlet weak var addNoteTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
     
     var delegate: AddNoteDelegate?
+    private var datePicker = UIDatePicker()
 
     init(delegate: AddNoteDelegate) {
         self.delegate = delegate
@@ -31,11 +33,50 @@ class AddNoteVC: UIViewController {
         super.viewDidLoad()
         addNoteTextField.delegate = self
         addNoteTextField.enablesReturnKeyAutomatically = true
-        let accessoryView = Bundle.main.loadNibNamed("TextFieldAccessoryView", owner: self, options: nil)?.first as! UIView
+        dateTextField.borderStyle = .none
+        dateTextField.tintColor = .clear
+        setUpDatePicker()
+//        setUpAccessoryView()
+    }
+
+    private func setUpAccessoryView() {
+        guard let nib = Bundle.main.loadNibNamed("TextFieldAccessoryView", owner: self, options: nil),
+            let accessoryView = nib[0] as? UIView else {
+                return
+        }
         accessoryView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 60)
         addNoteTextField.inputAccessoryView = accessoryView
     }
+
+    private func setUpDatePicker(){
+        datePicker.datePickerMode = .date
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneButtonTapped))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonTapped))
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        dateTextField.inputAccessoryView = toolbar
+        dateTextField.inputView = datePicker
+    }
+
+    @objc private func doneButtonTapped(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        dateTextField.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+
+    @objc private func cancelButtonTapped(){
+        self.view.endEditing(true)
+    }
+
     @IBAction func priorityButtonTapped(_ sender: UIButton) {
+        print(sender.titleLabel?.text)
+    }
+    
+    @IBAction func setDateButtonTapped(_ sender: UIButton) {
+        dateTextField.becomeFirstResponder()
         print(sender.titleLabel?.text)
     }
 }
@@ -52,5 +93,6 @@ extension AddNoteVC: UITextFieldDelegate {
         }
         return true
     }
+
 }
 
