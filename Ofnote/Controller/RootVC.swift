01374c74c2e6a77e77c6b407bd30b9c62cec65f3
Matt Deuschle <mdeuschle@gmail.com>
2018-05-13 08:20:25 -0700

    filtered search

--- Ofnote/Controller/RootVC.swift
+++ Ofnote/Controller/RootVC.swift
@@ -11,14 +11,19 @@ import UIKit
 class RootVC: SwipeVC, AddNoteDelegate {
 
     private var notes = [Note]()
+    private var filteredNotes = [Note]()
+    private var isFiltering = false
 
+    //MARK: Lifecyle
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.separatorStyle = .none
         setUpAddNoteButton()
         loadNotes()
+        setUpSearchBar()
     }
 
+    //MARK: Private methods
     private func loadNotes() {
         notes = Dao().unarchiveNotes() ?? [Note]()
         tableView.reloadData()
@@ -41,7 +46,7 @@ class RootVC: SwipeVC, AddNoteDelegate {
         navigationController?.pushViewController(addNoteVC, animated: true)
     }
 
-    // AddNote Delegate Method
+    //MARK: AddNote Delegate Method
     func add(note: Note) {
         notes.append(note)
         Dao().archive(notes: notes)
@@ -49,29 +54,54 @@ class RootVC: SwipeVC, AddNoteDelegate {
         tableView.reloadData()
     }
 
+    //MARK: Remove note
     override func updateModel(at indexPath: IndexPath) {
         notes.remove(at: indexPath.row)
         Dao().archive(notes: notes)
     }
 
-    // TableView DataSource Methods
+    //MARK: TableView DataSource Methods
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notes.count
+        return isFiltering ? filteredNotes.count : notes.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
-        let note = notes[indexPath.row]
+        let row = indexPath.row
+        let note = isFiltering ? filteredNotes[row] : notes[row]
         cell.textLabel?.text = note.title
         cell.backgroundColor = Priority(rawValue: note.priority)?.color()
         return cell
     }
 }
 
+extension RootVC: UISearchControllerDelegate, UISearchResultsUpdating {
+
+    private func setUpSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.delegate = self
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+    }
+
+    func updateSearchResults(for searchController: UISearchController) {
+        searchController.dimsBackgroundDuringPresentation = false
+        if let text = searchController.searchBar.text, !text.isEmpty {
+            isFiltering = true
+            filteredNotes = notes.filter {
+                $0.title.lowercased().contains(text.lowercased())
+            }
+        } else {
+            isFiltering = false
+            filteredNotes = notes
+        }
+        tableView.reloadData()
+    }
+}
 
 
 
